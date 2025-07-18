a4c28baff30d00e28280237f3c5fb5361cfe2832
Manjunath Bhandari <bhandarimanjunath98@gmail.com>
2025-07-18 13:40:50 +0530

    final fix

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -13,18 +13,18 @@ export const experience = [
     ],
     techUsed: ["Shopify", "JavaScript", "CSS", "Liquid", "HTML"],
   },
-  {
-    role: "Front-End Developer",
-    company: "DEVAPPSYS",
-    from: "Feb 2023",
-    to: "May 2023",
-    location: "Mangalore",
-    desc: [
-      "Developed responsive interfaces using React and modern JavaScript",
-      "Worked closely with designers and backend teams to deliver polished user experiences",
-      "Implemented pixel-perfect designs with attention to detail",
-      "Optimized applications for maximum speed and scalability",
-    ],
-    techUsed: ["React.js", "JavaScript", "CSS", "HTML", "Git"],
-  },
+  // {
+  //   role: "Front-End Developer",
+  //   company: "DEVAPPSYS",
+  //   from: "Feb 2023",
+  //   to: "May 2023",
+  //   location: "Mangalore",
+  //   desc: [
+  //     "Developed responsive interfaces using React and modern JavaScript",
+  //     "Worked closely with designers and backend teams to deliver polished user experiences",
+  //     "Implemented pixel-perfect designs with attention to detail",
+  //     "Optimized applications for maximum speed and scalability",
+  //   ],
+  //   techUsed: ["React.js", "JavaScript", "CSS", "HTML", "Git"],
+  // },
 ];
